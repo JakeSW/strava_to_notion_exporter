88bce5274b561aceed4887cb77d308fe859913e0
JakeSW <jakestuchbury@gmail.com>
2022-09-16 15:36:01 +0100

    added sync last date from notion

---
+++ notion_api_date.py
@@ -0,0 +1,36 @@
+from notion_client import Client
+from config import TOKEN_V3, DATABASE_ID
+
+# You need to allow the integration access to the page***
+
+notion = Client(auth=TOKEN_V3)
+
+#use this DatabasesEndpoint Class
+# Documentation from Notion:
+# https://developers.notion.com/reference/query-a-database
+# Use this to look at data in a notion database
+# notion.database.query
+
+# Here we look at the most recent item and take the date from the strave activity
+# then we can look at dates after this activity to sync
+def most_recent_activity_date(id=DATABASE_ID):
+  # query
+  data = notion.databases.query(database_id=id,page_size=1)
+
+  # find the date
+  results = data['results']
+  result_data = dict(results[0])
+  properties = result_data['properties']
+  Date = properties["Date"]
+  date = Date['date']
+  start_date = date['start']
+  start_date = start_date[0:10]
+  print(start_date)
+
+  #add a day to not duplicate, may miss double days though
+  end_char = int(start_date[-1])+1
+  start_date = start_date[0:9]+str(end_char)
+  print(start_date)
+
+  return start_date
+
--- strava_api.py
+++ strava_api.py
@@ -5,6 +5,8 @@ from stravaio import StravaIO
 
 from config import CLIENT_ID, CLIENT_SECRET, DATABASE_ID, All_Data
 from notion_api_new import uploadToNotion
+from notion_api_date import most_recent_activity_date
+
 
 token = strava_oauth2(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 
@@ -12,40 +14,18 @@ token = strava_oauth2(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 client = StravaIO(access_token=token["access_token"])
 print("Authorised")
 
+# Check if we sync all activities or activities after a the last date
 if All_Data:
      after_date=0
 else:
-     with open('last.txt', 'r') as file:
-          after_date = file.read().rstrip()
+     after_date = most_recent_activity_date()
 
 activities = client.get_logged_in_athlete_activities(after=after_date)
 print("Activities fetched")
 
-print(activities[-1].id)
-
-# for activity in activities:
-#     print(activity)
-#     break
-
-# Upload information to Notion
-#notion = NotionInterface()
-#table = notion.create_activity_log_table()
-#for activity in activities:
-#    notion.add_row_to_table(table, activity)
-
 parent = { "type": "database_id", "database_id": DATABASE_ID }
-
 
 for activity in activities:
     
      uploadToNotion(parent=parent, data=activity)
      print("Uploaded activity")
-
-# save sync time
-
-now = datetime.now()
-after_date = strftime("%Y-%m-%d")
-
-last_file = open("last.txt", "w")
-n = last_file.write(after_date)
-last_file.close()
